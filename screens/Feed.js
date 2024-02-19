17a2112443f0505284507f2d65e7bcaade0b0203
avinash badaramoni <badaramoni.avinash@gmail.com>
2024-02-19 12:24:08 -0500

    initial commit for social

---
+++ screens/Feed.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text ,View} from "react-native"
+import {  } from "react-native-safe-area-context"
+
+export default function Feed(){
+    return(
+        <SafeAreaView>
+          <Text>
+            Feed some text
+          </Text>
+        </SafeAreaView>
+    );
+}
